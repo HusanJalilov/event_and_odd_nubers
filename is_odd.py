ed651b6cd9a3a7911441e88d51a9e95b1f5acb43
coder_uz <husanjalilov00777@gmil.com>
2022-07-12 15:20:36 +0500

    odd

--- is_odd.py
+++ is_odd.py
@@ -1,7 +1,5 @@
 #Create a variable "number" and assign it a integer value.
 number=11
-if number%2:
-    print(0)
-else:
-    print(1)
+
+print(int(bool(number%2)))
 #Print 1 if the number is odd, otherwise print 0.
