88dd2e12a9e59acf81350669b7d016be0a3e5ef5
coder_uz <husanjalilov00777@gmil.com>
2022-07-12 16:54:10 +0500

    odd task

--- number_odd.py
+++ number_odd.py
@@ -1,14 +1,15 @@
 #A four-digit integer is given. Find the number of odd digits in it.
-number=1551
+var_int=1000
 #Create a variable "var_int" and assign it a four-digit integer value.
-x1=number%10
-number=number//10
-x2=number%10
-number=number//10
-x3=number%10
-number=number//10
-x4=number%10
-number=number//10
+x1=var_int%10
+var_int=var_int//10
+x2=var_int%10
+var_int=var_int//10
+x3=var_int%10
+var_int=var_int//10
+x4=var_int%10
+var_int=var_int//10
 #Print the number of odd digits in the variable "var_int"
-var_int=x1%2+x2%2+x3%2+x4%2
-print(var_int)
+
+s=x1%2+x2%2+x3%2+x4%2
+print(s)
