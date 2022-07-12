45a328fb627b32d17bcad13fb4977e264fd2f904
coder_uz <husanjalilov00777@gmil.com>
2022-07-12 16:05:28 +0500

    even

--- number_even.py
+++ number_even.py
@@ -1,14 +1,14 @@
 #A four-digit integer is given. Find the number of even digits in it.
-var_int=1234
+A=1244
 #Create a variable "var_int" and assign it a four-digit integer value.
-x1=var_int%10
-var_int=var_int//10
-x2=var_int%10
-var_int=var_int//10
-x3=var_int%10
-var_int=var_int//10
-x4=var_int%10
-var_int=var_int//10
-s=int(bool(x1%2))+int(bool(x2%2))+int(bool(x3%2))+int(bool(x4%2))
-print(s)
+x1=A%10
+A=A//10
+x2=A%10
+A=A//10
+x3=A%10
+A=A//10
+x4=A%10
+A=A//10
+var_int=int(not(bool(x1%2)))+int(not(bool(x2%2)))+int(not(bool(x3%2)))+int(not(bool(x4%2)))
+print(var_int)
 #Print the number of even digits in the variable "var_int".
